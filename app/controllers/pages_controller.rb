cfb277d447673d19e464ac1a102ff1b0f3c0e774
kvartik <kvartik0606@gmail.com>
2019-09-20 23:43:45 +0300

    errors

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+
+  def welcome
+    @title = 'Добро пожаловать!'
+  end
+  def about_us
+    @title = 'Мы учим людей программированию и делаем мир лучше!'
+  end
+
+end
